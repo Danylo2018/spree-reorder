435ed7fbfd449eabd38ce06ea5b535eb08bc7888
ezekg <ezekg@yahoo.com>
2017-01-17 16:50:32 -0600

    Refactor reorder method to support Spree 3.2

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,16 +1,15 @@
 Spree::OrdersController.class_eval do
   def reorder
-    populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
-    order = Spree::Order.where( number: params[:id] ).first
+    reorder = Spree::OrderContents.new current_order(create_order_if_necessary: true)
+    order = Spree::Order.find_by number: params[:id]
 
-    if order.line_items.select {|li| populator.populate(li.variant.id, li.quantity) }.any?
+    if order.line_items.select { |item| reorder.add(item.variant, item.quantity) }.any?
       respond_with(@order) do |format|
         format.html { redirect_to cart_path }
       end
     else
-      flash[:error] = populator.errors.full_messages.join(' '.freeze)
+      flash[:error] = reorder.errors.full_messages.join ' '
       redirect_to :back
     end
-
   end
 end
