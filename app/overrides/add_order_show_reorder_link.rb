f2da7c59777965cee0a7cc009e963fef9b7fba0b
ezekg <ezekg@yahoo.com>
2017-01-17 16:50:49 -0600

    Add original hash to deface override

--- app/overrides/add_order_show_reorder_link.rb
+++ app/overrides/add_order_show_reorder_link.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+  original:      "50cf273def294c450165810b59c759af5540083f".freeze,
   virtual_path:  "spree/orders/show".freeze,
   name:          "add_order_show_reorder_link".freeze,
   insert_bottom: "[data-hook='links']".freeze,
